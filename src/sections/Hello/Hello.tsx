a6aa7edf946a6f5c6d2150bc60472eeea7c8d7b7
Vishal Saini <vsaini17@hotmail.com>
2021-09-28 01:56:13 +0530

    add image in hello section

--- src/sections/Hello/Hello.tsx
+++ src/sections/Hello/Hello.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import { Box } from '@material-ui/core'
+import React from "react";
+import { Box, Container, Grid, Typography } from "@material-ui/core";
+import user from "../../assets/img_avatar.png";
 
-type props = {}
+type props = {};
 
 const Hello: React.FC<props> = () => {
+  const userName = "Vishal Saini";
 
   return (
-    <>
-      <Box component="section" id="hello">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate magnam aperiam rerum nulla illum harum itaque vero eius, repellendus repellat et a inventore eum placeat obcaecati sapiente, odit distinctio aliquid.
-      </Box>
-    </>
-  )
-}
-export default Hello
+    <Box component="header" id="hello">
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={3}>
+            <img
+              src={user}
+              alt={userName}
+              style={{
+                borderRadius: "5px",
+                boxShadow: "0 0 27px rgb(96 96 96 / 34%)",
+                width: "90%",
+                height: "380px",
+                objectFit: "cover",
+                marginTop: "10px",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography variant="h1">{userName}</Typography>
+            <Typography variant="h6">
+              frontend developer, QA-engineer
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+export default Hello;
